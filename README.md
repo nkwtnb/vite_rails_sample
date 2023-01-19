9eb50561d127f2f413be61d3204a9f0a5037fb97
nkwtnb <n.watanabe0305@gmail.com>
2023-01-19 19:04:56 +0900

    動作確認

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # vite_rails_sample
-vite_railsを使用したサンプルリポジトリ
+- vite_railsを使用したサンプルリポジトリ  
+- `React`や`tailwind CSS`を使用したモダンな開発環境
+- バックエンドとフロントエンドは同じサーバー（バックエンド統合）の構成
+
+## [vite_rails](https://github.com/ElMassimo/vite_ruby/tree/main/vite_rails)
+- Railsのフロントエンド開発にviteを組み込むgem
+
+## 参考
+- [【Vite Rails】RailsアプリにViteを導入したい](https://zenn.dev/oreo2990/articles/fe8160437ed089)
---
+++ app/javascript/entrypoints/App.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+const App = () => {
+    return (
+        <div>
+            <h1 className="text-5xl font-bold underline">
+                Hello world
+                <p className="italic ...">The quick brown fox ...</p>
+                <p className="not-italic ...">The quick brown fox ...</p>
+            </h1>
+        </div>
+    )
+}
+
+export default App
